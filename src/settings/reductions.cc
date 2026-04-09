2d38da4e75ec57c4718ade5da57efeb8ad7ff692
Jaehyeok Choi <jaehyeok@iastate.edu>
2026-04-09 14:19:54 -0500

    ok

--- src/settings/reductions.cc
+++ src/settings/reductions.cc
@@ -13,7 +13,7 @@ Reductions::Reductions()
 {
     dimension = 1;
     type = REX;
-    rules = std::vector<bool>(15,1);
+    rules = std::vector<bool>(11,1);
     rules[RULE_I0] = 0;
     rules[RULE_I1] = 0;
 }
